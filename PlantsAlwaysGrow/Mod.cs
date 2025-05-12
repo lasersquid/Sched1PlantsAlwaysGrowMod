dfc97cee1f9dfc01ac74646e4ad5b0b8a89ef1f5
levit <levit@DESKTOP-96BFOCF>
2025-05-11 18:30:52 -0600

    Add project files.

---
+++ PlantsAlwaysGrow/Mod.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using MelonLoader;
+using HarmonyLib;
+
+
+[assembly: MelonInfo(typeof(PlantsAlwaysGrow.PlantsAlwaysGrowMod), "PlantsAlwaysGrowMod", "1.0.0", "lasersquid", null)]
+[assembly: MelonGame("TVGS", "Schedule I")]
+
+namespace PlantsAlwaysGrow
+{
+    public class PlantsAlwaysGrowMod : MelonMod
+    {
+        private static bool _isPatchApplied;
+
+        private static HarmonyLib.Harmony _harmonyInstance;
+
+        public override void OnInitializeMelon()
+        {
+            MelonEvents.OnSceneWasLoaded.Subscribe(OnSceneLoaded);
+            LoggerInstance.Msg("EmployeesAlwaysWorkWithoutBedsMod Initialized.");
+        }
+
+        private void OnSceneLoaded(int buildIndex, string sceneName)
+        {
+            if (sceneName == "Main" && !_isPatchApplied)
+            {
+                try
+                {
+                    _harmonyInstance = new HarmonyLib.Harmony("PlantsAlwaysGrowMod");
+                    _harmonyInstance.PatchAll();
+                    _isPatchApplied = true;
+                    base.LoggerInstance.Msg("Plants Always Grow successfully initialized in scene: " + sceneName);
+                }
+                catch (Exception ex)
+                {
+                    base.LoggerInstance.Error("Failed to patch mod: " + ex.Message);
+                }
+            }
+        }
+    }
+
+
+
+}
+
---
+++ PlantsAlwaysGrow/PlantsAlwaysGrow.cs
@@ -0,0 +1,43 @@
+﻿using HarmonyLib;
+
+#if MONO_BUILD
+using ScheduleOne.DevUtilities;
+using ScheduleOne.GameTime;
+using ScheduleOne.Growing;
+#else
+using Il2CppScheduleOne.DevUtilities;
+using Il2CppScheduleOne.GameTime;
+using Il2CppScheduleOne.Growing;
+#endif
+
+namespace PlantsAlwaysGrow
+{
+    [HarmonyPatch(typeof(Plant), "MinPass")]
+    public static class PlantMinPassPatch
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(Plant __instance)
+        {
+            if (__instance.NormalizedGrowthProgress >= 1f)
+            {
+                return false;
+            }
+            TimeManager timeManager = NetworkSingleton<TimeManager>.Instance;
+            if ((UnityEngine.Object)(object)timeManager == null || !timeManager.IsEndOfDay)
+            {
+                return true;
+            }
+            float num = 1f / ((float)__instance.GrowthTime * 60f);
+            num *= __instance.Pot.GetAdditiveGrowthMultiplier();
+            num *= __instance.Pot.GetAverageLightExposure(out var growSpeedMultiplier);
+            num *= __instance.Pot.GrowSpeedMultiplier;
+            num *= growSpeedMultiplier;
+            if (__instance.Pot.NormalizedWaterLevel <= 0f || __instance.Pot.NormalizedWaterLevel > 1f)
+            {
+                num *= 0f;
+            }
+            __instance.SetNormalizedGrowthProgress(__instance.NormalizedGrowthProgress + num);
+            return false;
+        }
+    }
+}
